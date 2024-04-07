f57625ca8091a8407df1f9d23470ead573cc66b6
maajidmuhammad <abdimohamed0742@gmail.com>
2024-04-07 17:55:32 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
+  // Convert the word to lowercase
+  word = word.toLowerCase();
 
-/* 
-  Add your pseudocode here
-*/
+  // Initialize pointers for the start and end of the word
+  let left = 0;
+  let right = word.length - 1;
 
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+  // Iterate until the pointers meet in the middle
+  while (left < right) {
+    // If characters at the pointers are not equal, it's not a palindrome
+    if (word[left] !== word[right]) {
+      return false;
+    }
+    // Move the pointers towards the middle
+    left++;
+    right--;
+  }
+  
+  // If the loop completes without returning false, the word is a palindrome
+  return true;
 }
 
 module.exports = isPalindrome;
